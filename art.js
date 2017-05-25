7f1f75eea00f5f40ee2f011e24d4e2215f45d8a0
Mrinalini Sinha <ms6471@nyu.edu>
2017-05-25 06:16:07 +0530

    got rid of the useless jquery bit

--- art.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#first").mouseover(function() {
-    $("#greenlady").replaceWith('<img src="images/greenlady1.png" style="width: 100%;">')
-  });
-  $("#first").mouseout(function() {
-    $("#greenlady").replaceWith('<img src="images/mrinsin1.png" style="width: 100%;">')
-  });
-})
